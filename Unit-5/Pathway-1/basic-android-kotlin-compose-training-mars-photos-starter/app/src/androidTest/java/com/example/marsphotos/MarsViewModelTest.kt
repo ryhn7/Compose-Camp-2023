d498c3fb9b6e9fd6b76ce69d786f316da7fb9871
Rayhan Illyas <rayhan205732@gmail.com>
2023-10-31 14:11:33 +0700

    done test view model

--- Unit-5/Pathway-1/basic-android-kotlin-compose-training-mars-photos-starter/app/src/androidTest/java/com/example/marsphotos/MarsViewModelTest.kt
+++ Unit-5/Pathway-1/basic-android-kotlin-compose-training-mars-photos-starter/app/src/androidTest/java/com/example/marsphotos/MarsViewModelTest.kt
@@ -1,20 +1,33 @@
 package com.example.marsphotos
 
 import com.example.marsphotos.fake.FakeMarsPhotoRepositoryImpl
+import com.example.marsphotos.rules.TestDispatcherRule
+import com.example.marsphotos.ui.screens.MarsUiState
 import com.example.marsphotos.ui.screens.MarsViewModel
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
 
 class MarsViewModelTest {
+    @get:Rule
+    val testDispatcher = TestDispatcherRule()
+
     @Test
     fun marsViewModel_getMarsPhotos_verifyMarsUiStateSuccess() =
         runTest {
             // Given
             val marsPhotoRepository = FakeMarsPhotoRepositoryImpl()
             val marsViewModel = MarsViewModel(marsPhotoRepository)
+            val listResult = marsPhotoRepository.getPhotos()
 
 
             // When
-//            val marsUiStateSuccess =
+            val marsUiStateSuccess = marsViewModel.marsUiState
+
+            // Then
+            assertEquals(
+                MarsUiState.Success(listResult), marsUiStateSuccess
+            )
         }
 }
